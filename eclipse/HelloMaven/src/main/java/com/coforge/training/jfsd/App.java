dda3a995b377172b2ad85124ee8d0ceef449f712
sarvada <kumbharsarvada@gmail.com>
2021-12-01 20:31:28 +0530

    Javascrpt file

---
+++ eclipse/HelloMaven/src/main/java/com/coforge/training/jfsd/App.java
@@ -0,0 +1,18 @@
+package com.coforge.training.jfsd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! from Maven Project" );
+        System.out.println();
+        
+         Student s1=new Student(101,"james ","Btech",7889);
+        s1.display();
+        
+    }
+}
